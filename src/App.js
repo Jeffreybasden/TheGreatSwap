dc77be61a21500781b8799fb78d1a049e387bc14
Jeddbasden <jeddbasden2@gmail.com>
2022-05-16 23:18:26 -0500

    moved functions inside component

--- src/App.js
+++ src/App.js
@@ -2,33 +2,33 @@ import react from 'react'
 import { useState } from 'react';
 import { ethers } from 'ethers';
 
-async function connect(){
-  const [isConnected, setIsConnected] = useState(false)
-
-  if(window.ethereum){
-    await window.ethereum.request({method:'eth_requestAccounts'})
-    return 
-  }else window.alert('Please add metamask extension!')
-}
-
-async function swap(){
-
-}
-
-
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+
+  async function connect() {
+    if(window.ethereum){
+      await window.ethereum.request({method:'eth_requestAccounts'})
+      return ;
+    }else window.alert('Please add metamask extension!')
+  }
+
+  async function swap(){
+
+  }
+
+
   return (
     <div>
       <div>
         <div>
-          <input type='input'/>
+          <input type='input' />
         </div>
         <div>
           <p></p>
         </div>
-        <button onClick={connect}>Connect</button>
-        <button></button>
+        {isConnected && <button>Connected!</button>}
+        {!isConnected && <button onClick={connect}></button>}
       </div>
     </div>
   );
